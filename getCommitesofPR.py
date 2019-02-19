4cdd522f99bca8de68e8e4a138584ea76c2cb72a
Shiyang HUANG <shiyang.huang@etu.unice.fr>
2019-02-19 10:55:11 +0100

    treat the failed commit as the same commit

--- getCommitesofPR.py
+++ getCommitesofPR.py
@@ -1,40 +1,66 @@
 from github import Github
 from utils import *
-# clauses :
+import sys
+import getopt
 
-# code qui
-
-print("start get commits of pull")
-g = Github("9517082cce9860b93b7117f40989b1a92de39de9")
-repo = g.get_repo("SonarSource/sonarqube")
-myCommits = []
+NBtestsPasAJour = 0
+NBtestsMalEcrit = 0
+NBcodeAjouteMauvais = 0
+NBcodeAjouteMarche = 0
 
 
-def get_commits(numeroPull):
-    global myCommits
+def main(argv):
+    repo_name = ''
+    inputfile = ''
+    helpMsg = "test.py -r <github/repo> -i <inputfile>"
+    try:
+        opts, args = getopt.getopt(argv, "hr:i:", ["repo=", "ifile="])
+    except getopt.GetoptError:
+        print(helpMsg)
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print(helpMsg)
+            sys.exit()
+        elif opt in ("-r", "--repo"):
+            repo_name = arg
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+    if repo_name == '' or inputfile == '':
+        print(helpMsg)
+        sys.exit(2)
+
+    print("start get commits of pull")
+    g = Github("9517082cce9860b93b7117f40989b1a92de39de9")
+    repo = g.get_repo(repo_name)
+    with open(inputfile, "r") as ins:
+        for line in ins:
+            commits = get_commits(g, repo, int(line))
+            hisCodeIsBad(commits, int(line))
+    print("----------------------resultat:--------------------")
+    print("NBtestsPasAJour:" + str(NBtestsPasAJour) + " NBtestsMalEcrit:" + str(NBtestsMalEcrit) +
+          " NBcodeAjouteMauvais:" + str(NBcodeAjouteMauvais) + " NBcodeAjouteMarche:" + str(NBcodeAjouteMarche))
+
+
+def get_commits(g, repo, numeroPull):
     myCommits = []
     pr = repo.get_pull(numeroPull)
     commits = pr.get_commits()
+    i = 0
     for c in commits:
         status = "good"
         filesModified = []
         wait_limit_reset(g, 100)
-        if is_build_fail(c):
-            status = "error"
         for f in c.files:
             filesModified.append(f.filename)
-        myCommits.append(
-            {"commite": c, "status": status, "files": filesModified})
+        if is_build_fail(c):
+            status = "error"
+            if i is not 0 and myCommits[i-1]["status"] == "error":
+                myCommits[i - 1]["files"].extend(filesModified)
+                continue
+        myCommits.append({"status": status, "files": filesModified})
+        i += 1
     return myCommits
-
-
-def run():
-    with open("had_failed_pull_requests_number.txt", "r") as ins:
-        for line in ins:
-            hisCodeIsBad(int(line))
-    print("----------------------resultat:--------------------")
-    print("NBtestsPasAJour:" + str(NBtestsPasAJour) + " NBtestsMalEcrit:" + str(NBtestsMalEcrit) +
-          " NBcodeAjouteMauvais:" + str(NBcodeAjouteMauvais) + " NBcodeAjouteMarche:" + str(NBcodeAjouteMarche))
 
 
 def testsPasMisAJour(array1, array2):
@@ -83,46 +109,36 @@ def codeAjouteMarche2(before, errorCommit, after):
     return codeAjouteMarche(before, after)
 
 
-NBtestsPasAJour = 0
-NBtestsMalEcrit = 0
-NBcodeAjouteMauvais = 0
-NBcodeAjouteMarche = 0
-
-
-def hisCodeIsBad(pullrequest):
+def hisCodeIsBad(commits, pullrequest):
     print("####" + str(pullrequest))
-    get_commits(pullrequest)
     global NBtestsPasAJour
     global NBtestsMalEcrit
     global NBcodeAjouteMauvais
     global NBcodeAjouteMarche
-    for i in range(len(myCommits) - 1):
-        if(myCommits[i]["status"] == "error"):
-            if testsPasMisAJour(myCommits[i]["files"], myCommits[i+1]["files"]):
+    for i in range(len(commits) - 1):
+        if commits[i]["status"] == "error":
+            if testsPasMisAJour(commits[i]["files"], commits[i+1]["files"]):
                 NBtestsPasAJour += 1
                 print("Tests pas mis à jour")
-            if testsMalEcrit(myCommits[i]["files"], myCommits[i+1]["files"]):
+            if testsMalEcrit(commits[i]["files"], commits[i+1]["files"]):
                 NBtestsMalEcrit += 1
                 print("Tests mal écrit")
             CAD2 = False
             if i != 0:
                 CAD2 = codeAjouteDefectueux2(
-                    myCommits[i-1]["files"], myCommits[i]["files"], myCommits[i+1]["files"])
-            if codeAjouteDefectueux(myCommits[i]["files"], myCommits[i+1]["files"]) or CAD2:
+                    commits[i-1]["files"], commits[i]["files"], commits[i+1]["files"])
+            if codeAjouteDefectueux(commits[i]["files"], commits[i+1]["files"]) or CAD2:
                 NBcodeAjouteMauvais += 1
                 print("Le code ajouter contient des erreurs")
             CAM2 = False
             if i != 0:
                 CAM2 = codeAjouteMarche2(
-                    myCommits[i-1]["files"], myCommits[i]["files"], myCommits[i+1]["files"])
-            if codeAjouteMarche(myCommits[i]["files"], myCommits[i+1]["files"]) or CAM2:
+                    commits[i-1]["files"], commits[i]["files"], commits[i+1]["files"])
+            if codeAjouteMarche(commits[i]["files"], commits[i+1]["files"]) or CAM2:
                 NBcodeAjouteMarche += 1
                 print(
                     "Le code ajouter ne contient pas erreurs mais produit des erreurs dans le reste du system")
-    # print("----------------------resultat:--------------------")
-    #print("NBtestsPasAJour:" + str(NBtestsPasAJour) + " NBtestsMalEcrit:"+ str(NBtestsMalEcrit) + " NBcodeAjouteMauvais:"+ str(NBcodeAjouteMauvais) + " NBcodeAjouteMarche:"+ str(NBcodeAjouteMarche))
 
 
-# hisCodeIsBad(2924)
-
-run()
+if __name__ == "__main__":
+    main(sys.argv[1:])
